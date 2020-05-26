c943942b493f0ec87d22e2dda10d1eeeec352e86
marfavasilevna <bertosh_m@mail.ru>
2020-05-27 00:49:40 +0300

    Notepad v. 1, working

--- link.rb
+++ link.rb
@@ -1,14 +1,24 @@
 class Link < Post
 
   def initialize
+    # Вызываем конструктор родителя
     super
-
     @url = ''
   end
 
   def read_from_console
+    # Спрашиваем у пользователя url ссылки и записываем в переменную @url
+    puts 'Адрес ссылки (url):'
+    @url = STDIN.gets.chomp
+
+    # Спрашиваем у пользователя описание ссылки (одной строчки будет достаточно)
+    puts 'Что за ссылка?'
+    @text = STDIN.gets.chomp
   end
 
   def to_strings
+    time_string = "Создано: #{@created_at.strftime('%Y.%m.%d, %H:%M:%S')} \n\r"
+
+    [@url, @text, time_string]
   end
 end
